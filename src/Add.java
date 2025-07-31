86888bc6d57f7861013d7294433c39fc028b418e
unknown <vaishnavireddymaddi@gmail.com>
2025-07-31 18:01:21 +0530

    Local changes before rebase

--- src/Add.java
+++ src/Add.java
@@ -1,5 +1,5 @@
 public class Add{
 public static int add(int x, int y){
-return 2*x+2*y;
+return 4*x+4*y;
 }
 }
--- src/Difference.java
+++ src/Difference.java
@@ -1,5 +1,5 @@
 public class Difference{
 public static int difference(int x,int y){
-return 2*x-2*y;
+return 4*x-4*y;
 }
 }
